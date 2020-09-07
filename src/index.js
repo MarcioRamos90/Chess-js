3b703661dffac07214b3f5e9bbbd14c49f7cb6a0
Marcio Ramos <marcio.ramos@tangotech.com.br>
2020-09-07 15:21:52 -0300

    first commit - Init config - Create board

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function game() {
+    const game = {};
+
+    game.state = {
+        
+    }
+
+    function start() {
+        // position the pieces
+    }
+
+    function stop() {}
+
+    function reset() {}
+
+    function save() {}
+
+    game.start;
+    game.stop;
+    game.reset;
+    game.save;
+
+    return game;
+}
+
+module.export = {
+    game,
+};
